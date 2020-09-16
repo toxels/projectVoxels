958bf934334b2264d5f9cc370f95f0839c4c51f5
hardworkar <arsmoroz@ya.ru>
2020-09-16 11:29:18 +0700

    all files lol

---
+++ cmake-build-debug/CMakeFiles/projectVoxels.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectVoxels.dir/main.cpp.obj"
+  "projectVoxels.pdb"
+  "projectVoxels.exe"
+  "projectVoxels.exe.manifest"
+  "libprojectVoxels.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projectVoxels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
